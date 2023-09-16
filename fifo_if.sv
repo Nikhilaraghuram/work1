d64f41ba9a4f56b1c34f89c57c70aceae79f74d4
Nikhila <108714280+Nikhilaraghuram@users.noreply.github.com>
2023-09-16 11:56:22 +0530

    Update fifo_if.sv

--- fifo_if.sv
+++ fifo_if.sv
@@ -2,8 +2,8 @@
 
 interface fifo_if(input bit clk, rstn);
     logic i_wren,i_rden;
-    logic  [DATA_W - 1 : 0] i_wrdata;
-    logic [DATA_W - 1 : 0] o_rddata;
+    logic  [`DATA_W - 1 : 0] i_wrdata;
+    logic [`DATA_W - 1 : 0] o_rddata;
     logic  o_alm_full , o_full,  o_alm_empty, o_empty;
 
     //clocking block for driver 
