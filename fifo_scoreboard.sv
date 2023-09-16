65fae0b10a6a8bb44dad10a4e36b649a4857be4f
Nikhila <108714280+Nikhilaraghuram@users.noreply.github.com>
2023-09-16 13:04:35 +0530

    Update fifo_scoreboard.sv

--- fifo_scoreboard.sv
+++ fifo_scoreboard.sv
@@ -16,22 +16,67 @@ class fifo_scoreboard extends uvm_scoreboard;
   endfunction
   
   bit[`DATA_W-1:0] queue[$];
+  bit[ 10:0] counter;
   
   function void write(input fifo_transaction trans);
     bit [`DATA_W-1:0] examdata;
+    bit ref_full;
+    bit ref_empty;
+    bit ref_alm_empty;
+    bit ref_alm_full;
     
     
-    if(trans.i_wren == 1'b1 && trans.i_rden==1'b0)begin
+    if(trans.i_wren == 1'b1 )begin
       queue.push_back(trans.i_wrdata);
-       `uvm_info("write Data", $sformatf("wren: %0b rden: %0b wrdata: %0h full:%0b alm_full:%0b",trans.i_wren, trans.i_rden,trans.i_wrdata, trans.o_full,trans.o_alm_full), UVM_LOW);
-    end
+      counter ++;
+      
+      if( queue.size() == `DEPTH)
+        ref_full = 1'b1;
+      
+      if( queue.size() >= (`DEPTH - `UPP_TH-1) && queue.size() < `DEPTH)
+        ref_alm_full=1'b1;
+      
+      if(ref_full)begin
+        $display(" reference is full");
+        if(ref_full == trans.o_full)
+          $display(" reference and design  are matched");
+      end
+      
+      if(ref_alm_full)begin
+        $display(" reference is almost full");
+        if(ref_alm_full == trans.o_alm_full)
+          $display(" reference and design  are matched");
+      end
+     
+      `uvm_info("write Data", $sformatf("wren: %0b rden: %0b wrdata: %0h full:%0b alm_full:%0b, counter = %0d",trans.i_wren, trans.i_rden,trans.i_wrdata, trans.o_full,trans.o_alm_full,counter), UVM_LOW);
+       
+    end 
+      
     
-    
-    
-  if  (trans.i_wren == 1'b0 && trans.i_rden==1'b1)begin
+  if  ( trans.i_rden==1'b1)begin
       if(queue.size() >= 'd1)begin
         examdata = queue.pop_front();
-        `uvm_info("Read Data", $sformatf("examdata: %0h data_out: %0h empty: %0b   alm_empty: %0b", examdata, trans.o_rddata, trans.o_empty, trans.o_alm_empty), UVM_LOW);
+        counter --;
+        
+        if( queue.size() == 0)
+        ref_empty = 1'b1;
+      
+        if( queue.size()>0 &&  queue.size() <= `LOW_TH)
+        ref_alm_empty=1'b1;
+        
+        if(ref_empty)begin
+          $display(" reference is empty");
+          if(ref_empty == trans.o_empty)
+          $display(" reference and design  are matched");
+      end
+      
+        if(ref_alm_empty)begin
+          $display(" reference is almost empty ");
+          if(ref_alm_empty == trans.o_alm_empty)
+          $display(" reference and design  are matched");
+      end
+     
+        `uvm_info("Read Data", $sformatf("examdata: %0h data_out: %0h empty: %0b   alm_empty: %0b, counter=%0d", examdata, trans.o_rddata, trans.o_empty, trans.o_alm_empty,counter), UVM_LOW);
         if(examdata == trans.o_rddata)begin
           $display("-------- 		Pass! 		--------");
         end
@@ -40,24 +85,7 @@ class fifo_scoreboard extends uvm_scoreboard;
           $display("--------		Check empty	--------");
         end
       end
-    end
-
-    
-if (trans.i_wren==1'b1 && trans.i_rden==1'b1)begin
-       queue.push_back(trans.i_wrdata);
-       `uvm_info("write and read", $sformatf("wren: %0b rden: %0b wrdata: %0h full: %0b  alm_full:%0b",trans.i_wren, trans.i_rden,trans.i_wrdata, trans.o_full,trans.o_alm_full), UVM_LOW);
-     if(queue.size() >= 'd1)begin
-         examdata = queue.pop_front();
-         `uvm_info("write and read", $sformatf("examdata: %0h data_out: %0h empty: %0b   alm_empty: %0b", examdata, trans.o_rddata, trans.o_empty, trans.o_alm_empty), UVM_LOW);
-         if(examdata == trans.o_rddata)begin
-           $display("-------- 		Pass! 		--------");
-         end
-         else begin
-           $display("--------		Fail!		--------");
-           $display("--------		Check empty	--------");
-         end
-       end
-     end 
+  end
 
   endfunction
   
