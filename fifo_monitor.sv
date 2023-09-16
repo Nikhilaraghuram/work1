78c355b1945f7da7877efb6d63e46e8921306b06
Nikhila <108714280+Nikhilaraghuram@users.noreply.github.com>
2023-09-16 13:06:11 +0530

    Update fifo_monitor.sv

--- fifo_monitor.sv
+++ fifo_monitor.sv
@@ -28,46 +28,30 @@ class fifo_monitor extends uvm_monitor;
     forever begin
       @( posedge vif.mon.mon_cb)
 //monitor write
-      if( vif.mon.mon_cb.i_wren == 1  &&  vif.mon.mon_cb.i_rden == 0)begin
+      if( vif.mon.mon_cb.i_wren == 1 )begin
         trans=fifo_transaction::type_id::create("trans");
         $display("\n write enable  is high");
         trans.i_wrdata =  vif.mon.mon_cb.i_wrdata;
         trans.i_wren = 1'b1;
-        trans.i_rden = 1'b0;
+        trans.i_rden = vif.mon.mon_cb.i_rden;
         trans.o_alm_full =  vif.mon.mon_cb.o_alm_full;
         trans.o_full = vif.mon.mon_cb.o_full;
         ap.write(trans);
       end
        
     //monitor read  
-    if( vif.mon.mon_cb.i_wren == 0 &&  vif.mon.mon_cb.i_rden == 1)begin
+    if(   vif.mon.mon_cb.i_rden == 1)begin
         @(posedge  vif.mon.mon_cb)
         trans=fifo_transaction::type_id::create("trans");
         $display("\nRead enable  is high");
         trans.o_rddata =  vif.mon.mon_cb.o_rddata;
-        trans.i_wren = 1'b0;
+        trans.i_wren = vif.mon.mon_cb.i_wren;
         trans.i_rden= 1'b1;
         trans.o_alm_empty = vif.mon.mon_cb.o_alm_empty;
         trans.o_empty =  vif.mon.mon_cb.o_empty;
         ap.write(trans);
       end
-      
-     //monitor write read 
- if( vif.mon.mon_cb.i_wren == 1 &&  vif.mon.mon_cb.i_rden == 1)begin
-        @(posedge  vif.mon.mon_cb)
-          trans=fifo_transaction::type_id::create("trans");
-          $display("\n write and read");
-          trans.i_wrdata =  vif.mon.mon_cb.i_wrdata;                                                  
-          trans.i_wren = 1'b1;                                                                        
-          trans.i_rden = 1'b1; 
-          trans.o_rddata =  vif.mon.mon_cb.o_rddata;
-          trans.o_alm_full =  vif.mon.mon_cb.o_alm_full;                                              
-          trans.o_full = vif.mon.mon_cb.o_full;     
-          trans.o_alm_empty = vif.mon.mon_cb.o_alm_empty;
-          trans.o_empty =  vif.mon.mon_cb.o_empty;
-          ap.write(trans);
-
-       end
+   
 end
 endtask
 endclass
