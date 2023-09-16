7a93fa8d4360e381d54b6e977a63682c5a5fe478
Nikhila <108714280+Nikhilaraghuram@users.noreply.github.com>
2023-09-16 10:14:56 +0530

    Create fifo_define.sv

---
+++ fifo_define.sv
@@ -0,0 +1,4 @@
+`define DATA_W    128          // Data width
+`define DEPTH   1024             // Depth of FIFO                   
+`define UPP_TH  4           // Upper threshold to generate Almost-full
+`define LOW_TH   2   
